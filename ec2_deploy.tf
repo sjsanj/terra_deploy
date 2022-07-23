746a984dbdfb2a943b06604afde276fe3682d20f
sjsanj <107824358+sjsanj@users.noreply.github.com>
2022-07-23 14:51:24 +0530

    Update ec2_deploy.tf

--- ec2_deploy.tf
+++ ec2_deploy.tf
@@ -1,5 +1,5 @@
 module "ec2-instance" {
-    source = "https://github.com/sjsanj/terra_deploy.git"  
+    source = ""  
     ami_id = var.ami_id   
     instance_type = var.instance_type
     vpc_id = var.vpc_id   
